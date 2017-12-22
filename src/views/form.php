e3276f0d543b6c478819e60e5bb617a2b3c4913c
Jermaine Rattray <xclydes@gmail.com>
2017-12-22 17:36:11 -0500

    Update column rendering.

--- src/views/form.php
+++ src/views/form.php
@@ -18,32 +18,16 @@
     <?php foreach ($fields as $field): ?>
     	<?php if( ! in_array($field->getName(), $exclude) ) { ?>
             <?php
-                $isAgroup = $field->getOption('is_group', false);
                 //Set the ratio
                 $field->setOption('col_ratio', $colRatio);
-                //Reset previous columns
-                if( $isAgroup && $colIndex != 0 ) {
-                    $colIndex = 0;
-                    echo '</div>';
-                }
-                if( $colIndex == 0 ) {
-                    $rowIndex++;
-                    $grpClass = 'form-group';
-                    if( $rowIndex > 4 ) {
-                        $grpClass .= ' advanced';
-                    }
-                    echo '<div class="'.$grpClass.'">';
-                }
-                $colIndex++;
-
+                //Update the wrapper options
+                $wrapperAttrs = $field->getOption('wrapperAttrs', '');
+                //Append the column size
+                $wrapperAttrs .= 'class="col-sm-' . ($colRatio * 2) . '""';
+                //Update it
+                $field->setOption('wrapperAttrs', $wrapperAttrs);
             ?>
         	<?= $field->render() ?>
-            <?php
-                if( $isAgroup || $colIndex == $columns ) {
-                    $colIndex = 0;
-                    echo '</div>';
-                }
-            ?>
 		<?php } ?>
     <?php endforeach; ?>
     <?php
