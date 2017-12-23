cdaf5efc536a4cbffef516ea0fc2ef26c564f8ed
Jermaine Rattray <xclydes@gmail.com>
2017-12-23 11:10:56 -0500

    Correct entity generation.

--- src/Xclydes/Larva/FormEloquentTrait.php
+++ src/Xclydes/Larva/FormEloquentTrait.php
@@ -9,22 +9,22 @@ use Illuminate\Database\Eloquent\Model;
 use Xclydes\Larva\Metadata\TableData;
 
 trait FormEloquentTrait {
-	
-	private static $tableModels = [];
-	
-	/**
-	 * @return string[]
-	 */
-	protected function getProtectedFields() {
-		return [
-			Model::CREATED_AT,
-			Model::UPDATED_AT,
-			IFormEloquent::FIELD_DELETED_AT,
-			IFormEloquent::FIELD_CREATED_BY,
-			IFormEloquent::FIELD_UPDATED_BY,
-			IFormEloquent::FIELD_DELETED_BY,
-		];
-	}
+
+    private static $tableModels = [];
+
+    /**
+     * @return string[]
+     */
+    protected function getProtectedFields() {
+        return [
+            Model::CREATED_AT,
+            Model::UPDATED_AT,
+            IFormEloquent::FIELD_DELETED_AT,
+            IFormEloquent::FIELD_CREATED_BY,
+            IFormEloquent::FIELD_UPDATED_BY,
+            IFormEloquent::FIELD_DELETED_BY,
+        ];
+    }
 
     /**
      * Indicates whether or not the field specified
@@ -33,12 +33,12 @@ trait FormEloquentTrait {
      * in question.
      * @return boolean
      */
-	public function isDisplayedInForm( $fieldData ){
-		$fieldName = $fieldData->name;
-		//Assume the form is to be displayed
-		return !$this->isGuarded( $fieldName )
-			&& !in_array($fieldName, $this->getProtectedFields());
-	}
+    public function isDisplayedInForm( $fieldData ){
+        $fieldName = $fieldData->name;
+        //Assume the form is to be displayed
+        return !$this->isGuarded( $fieldName )
+            && !in_array($fieldName, $this->getProtectedFields());
+    }
 
     /**
      * Whether or not the field should be visible in the form.
@@ -47,14 +47,14 @@ trait FormEloquentTrait {
      * in question.
      * @return boolean
      */
-	public function isIncludedInForm($fieldData ){
-		$fieldName = $fieldData->name;
-		$inc = false;
-		$isFillable = empty( $this->fillable ) || in_array($fieldName, $this->fillable);
-		$isInternalField = in_array($fieldName, $this->getProtectedFields());
-		$inc = $fieldName && !$this->isGuarded( $fieldName ) && $isFillable && !$isInternalField;
-		return $inc;
-	}
+    public function isIncludedInForm($fieldData ){
+        $fieldName = $fieldData->name;
+        $inc = false;
+        $isFillable = empty( $this->fillable ) || in_array($fieldName, $this->fillable);
+        $isInternalField = in_array($fieldName, $this->getProtectedFields());
+        $inc = $fieldName && !$this->isGuarded( $fieldName ) && $isFillable && !$isInternalField;
+        return $inc;
+    }
 
     /**
      * Gets the initial list of options to be used
@@ -64,15 +64,15 @@ trait FormEloquentTrait {
      * @param TableColumn $fieldData The field to be checked.
      * @return mixed
      */
-	public function getFieldInitOptions($formFieldType, $fieldData) {
-		$opts = array();
-		//If the field type says entity
-		if( $formFieldType == 'entity' ) {
-		    /** @var $firstFKey  ForeignKey */
-		    $fKeys = array_values( $fieldData->foreignKeys );
-		    $firstFKey = array_shift( $fKeys );
-		    if( $firstFKey != null ) {
-		        //Get the related table
+    public function getFieldInitOptions($formFieldType, $fieldData) {
+        $opts = array();
+        //If the field type says entity
+        if( $formFieldType == 'entity' ) {
+            /** @var $firstFKey  ForeignKey */
+            $fKeys = array_values( $fieldData->foreignKeys );
+            $firstFKey = array_shift( $fKeys );
+            if( $firstFKey != null ) {
+                //Get the related table
                 $ownerTable = $firstFKey->ownerTableName;
                 //Take the first property
                 $remoteCols = array_values( $firstFKey->ownerColumns );
@@ -92,17 +92,28 @@ trait FormEloquentTrait {
                         $opts['property_key'] = $firstProp;
                         $nameField = $firstProp;
                         $clsChain = class_implements( $fqN );
-                        if( in_array( '\Xclydes\Larva\Contracts\IFormEloquent', $clsChain ) ) {
+                        logger()->debug("FQN : '{$fqN}, Class chain ", $clsChain);
+                        if( in_array( 'Xclydes\\Larva\\Contracts\\IFormEloquent', $clsChain ) ) {
                             $nameField = $fqN::getDescriptionField();
+                        } else {
+                            //Use the first text column
+                            foreach($ownerTableData->getColumns() as $ownerCol) {
+                                /** @var $ownerCol TableColumn */
+                                if( $ownerCol->isText ) {
+                                    //Use this field
+                                    $nameField = $ownerCol->name;
+                                    break;
+                                }
+                            }
                         }
                         $opts['property'] = $nameField;
                         logger()->debug("FQN : '{$fqN}, Key : '{$firstProp}', Name : '{$nameField}'", $opts);
                     }
                 }
             }
-		}
-		return $opts;
-	}
+        }
+        return $opts;
+    }
 
     /**
      * Gets the preferred field type for the column
@@ -111,26 +122,26 @@ trait FormEloquentTrait {
      * being processed.
      * @return string
      */
-	public function getPreferredFieldType( $fieldData ) {
-		//Return null to use default option
-		$prefType = null;
-		//If the field data has foreign key information
-		if( count( $fieldData->foreignKeys ) > 0 ) {
-		    //Get the list of classes
-		    $firstEntry = $fieldData->foreignKeys[0];
-			$foreignTableName = $firstEntry->ownerTableName;
-			$fqN = self::getForeignModel( $foreignTableName );
-			//If the FQN exists
-			if( class_exists( $fqN ) ) {
-				//Use the entity type				
-				$prefType = 'entity';
-			} else {
-				//This has to be a select type field
-				$prefType = 'select';				
-			}
-		} else {
-		    $fieldName = $fieldData->name;
-		    //If the dates attribute is set
+    public function getPreferredFieldType( $fieldData ) {
+        //Return null to use default option
+        $prefType = null;
+        //If the field data has foreign key information
+        if( count( $fieldData->foreignKeys ) > 0 ) {
+            //Get the list of classes
+            $firstEntry = $fieldData->foreignKeys[0];
+            $foreignTableName = $firstEntry->ownerTableName;
+            $fqN = self::getForeignModel( $foreignTableName );
+            //If the FQN exists
+            if( class_exists( $fqN ) ) {
+                //Use the entity type
+                $prefType = 'entity';
+            } else {
+                //This has to be a select type field
+                $prefType = 'select';
+            }
+        } else {
+            $fieldName = $fieldData->name;
+            //If the dates attribute is set
             if( property_exists($this, 'dates')
                 && is_array( $this->dates )
                 && in_array( $fieldName, $this->dates ) ) {
@@ -149,17 +160,17 @@ trait FormEloquentTrait {
                 }
             }
         }
-		return $prefType;
-	}
-	
-	/**
-	 * @param unknown $foreignTableName
-	 * @return Ambigous <NULL, string, mixed>
-	 */
-	private static function getForeignModel( $foreignTableName ) {
-		$fqClsName = null;
-		if( $foreignTableName ) {
-		    //Check the local cache
+        return $prefType;
+    }
+
+    /**
+     * @param unknown $foreignTableName
+     * @return Ambigous <NULL, string, mixed>
+     */
+    private static function getForeignModel( $foreignTableName ) {
+        $fqClsName = null;
+        if( $foreignTableName ) {
+            //Check the local cache
             $fqClsName = array_get(self::$tableModels, $foreignTableName);
             if( !$fqClsName ){
                 //Get the table data
@@ -177,8 +188,8 @@ trait FormEloquentTrait {
                     }
                 }
             }
-		}
-		logger()->debug("Table '{$foreignTableName}'' => Model '{$fqClsName}''");
-		return $fqClsName;
-	}
+        }
+        logger()->debug("Table '{$foreignTableName}' => Model '{$fqClsName}'");
+        return $fqClsName;
+    }
 } 
