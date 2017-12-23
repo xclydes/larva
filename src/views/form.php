8f4e5b8197abeda0f96b906d4ed7a9beac0f5a9e
Jermaine Rattray <xclydes@gmail.com>
2017-12-23 16:48:46 -0500

    Layout correction.

--- src/views/form.php
+++ src/views/form.php
@@ -1,15 +1,14 @@
 <?php if ($showStart): ?>
     <?= Form::open($formOptions) ?>
-<?php endif; ?>
-
-<?php if ($showFields): ?>
-
     <?php
     if( array_get( $formOptions, 'field_show_header', false) ) {
         //Render the header actions
         echo $form->getHeaderActionContainer()->render();
     }
     ?>
+<?php endif; ?>
+
+<?php if ($showFields): ?>
 
     <?php
     $colIndex = 0;
@@ -18,38 +17,39 @@
     ?>
     <?php foreach ($fields as $field): ?>
         <?php
-            if( ! in_array($field->getName(), $exclude) ) {
-                $isAgroup = $field->getOption('is_group', false);
-                if( $isAgroup && $colIndex != 0 ) {
-                    $colIndex = 0;
-                    echo $field_row_close;
-                }
-                if( $colIndex == 0 ) {
-                    $rowIndex++;
-                    echo $field_row_open;
-                }
-                $colIndex++;
-                echo $field->render();
-                if( $isAgroup || $colIndex == $field_column_count ) {
-                    $colIndex = 0;
-                    echo $field_row_close;
-                }
+        if( ! in_array($field->getName(), $exclude) ) {
+            $isAgroup = $field->getOption('is_group', false);
+            if( $isAgroup && $colIndex != 0 ) {
+                $colIndex = 0;
+                echo $field_row_close;
             }
+            if( $colIndex == 0 ) {
+                $rowIndex++;
+                echo $field_row_open;
+            }
+            $colIndex++;
+            echo $field->render();
+            if( $isAgroup || $colIndex == $field_column_count ) {
+                $colIndex = 0;
+                echo $field_row_close;
+            }
+        }
         ?>
     <?php
-        endforeach;
+    endforeach;
+    if( $colIndex != 0 ) {
         echo $field_row_close;
-    ?>
-
-    <?php
-    if( array_get( $formOptions, 'field_show_footer', true) ) {
-        //Render the actions
-        echo $form->getFooterActionContainer()->render();
     }
     ?>
 
 <?php endif; ?>
 
 <?php if ($showEnd): ?>
+    <?php
+    if( array_get( $formOptions, 'field_show_footer', true) ) {
+        //Render the actions
+        echo $form->getFooterActionContainer()->render();
+    }
+    ?>
     <?= Form::close() ?>
 <?php endif; ?>
