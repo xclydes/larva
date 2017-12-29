cf498d1511050935e0a7a913fe5c887a43f7e448
Jermaine Rattray <xclydes@gmail.com>
2017-12-29 18:34:33 -0500

    Remove conflicting methods in GridEloquentand FormEloquent traits.

--- src/Xclydes/Larva/GridEloquentTrait.php
+++ src/Xclydes/Larva/GridEloquentTrait.php
@@ -7,8 +7,6 @@ use Xclydes\Larva\Metadata\TableColumn;
 
 trait GridEloquentTrait {
 
-    private static $tableModels = [];
-
     /**
      * Gets the preferred value formatter for the column
      * specified.
@@ -27,8 +25,26 @@ trait GridEloquentTrait {
      * in question.
      * @return Column The column instance to be rendered.
      */
-    function getGridColumn( $fieldData ){
+    public function getGridColumn( $fieldData ) {
         return null;
+    }
+
+    /**
+     * Indicates whether or not the field specified
+     * should be added as field within the form.
+     * @param TableColumn $fieldData Details of the column
+     * in question.
+     * @return boolean
+     */
+    public function isDisplayedInGrid( $fieldData ) {
+        $fieldName = $fieldData->name;
+        $isProtected = false;
+        if( method_exists($this, 'getProtectedFields') ) {
+            $isProtected = in_array($fieldName, $this->getProtectedFields());
+        }
+        //Assume the form is to be displayed
+        return !$this->isGuarded( $fieldName )
+            && !$isProtected;
     }
 
     /**
@@ -36,14 +52,14 @@ trait GridEloquentTrait {
      * @return mixed The array of components to be
      * added.
      */
-    public function getGridComponents(){
+    public function getGridComponents() {
         return null;
     }
 
     /**
      * @return mixed
      */
-    public function getGridProviderOptions(){
+    public function getGridProviderOptions() {
         return null;
     }
 }
