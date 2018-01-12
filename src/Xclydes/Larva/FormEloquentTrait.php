829d1a1cd32c82605914821d3c692522b8ff68bf
Jermaine Rattray <xclydes@gmail.com>
2018-01-11 21:43:48 -0500

    Added getDropdownList convenience method.

--- src/Xclydes/Larva/FormEloquentTrait.php
+++ src/Xclydes/Larva/FormEloquentTrait.php
@@ -168,4 +168,40 @@ trait FormEloquentTrait {
         }
         return $prefType;
     }
+
+
+    /**
+     * Gets an array of entries from the model table.
+     * @param bool $id The field to be used as the ID. If not set the id field
+     * will be determined from the primary key.
+     * @param bool $descField The field to be used as the description. If not set,
+     * the getDescriptionField method will be called.
+     * @param bool $filterClosre A closure to which the query will be supplied
+     * for configuring the Builder.
+     * @return array The entries found keyed on the ID.
+     */
+    public static function getDropdownList($id = false, $descField = false, $filterClosre = false ) {
+        $items = [];
+        $fqn = get_called_class();
+        //Get the class
+        $ins = new $fqn;
+        //If it implements the form eloquent interface
+        if( $ins instanceof IFormEloquent ) {
+            if( !$descField ) {
+                $descField = $fqn::getDescriptionField();
+            }
+            $qry = $ins::whereNotNull($descField);
+            if( $filterClosre instanceof \Closure ) {
+                $filterClosre( $qry );
+            }
+            //If no ID is specified
+            if( !$id ) {
+                //Get the table data
+                $tblData = TableData::analyzeTable( $ins->getTable() );
+                $id = $tblData->getKeys()->first()->name;
+            }
+            $items = $qry->pluck($descField, $id);
+        }
+        return $items;
+    }
 } 
